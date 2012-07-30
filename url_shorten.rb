1d0422e809a58dfbcf15c847b7d426dd39a4b855
rufinomendoza <ThinkPadT400@Mobile>
2012-07-30 09:02:53 -0400

    Created branch with my changes

--- url_shorten.rb
+++ url_shorten.rb
@@ -1,4 +1,9 @@
+require 'rubygems'
 require 'sinatra'
+require 'sqlite3'
+
+db = SQLite3::Database.new('url.sql')
+db.execute( "create table if not exists url_shortener (id TEXT PRIMARY KEY, url TEXT);" )
 
 get '/new' do
   "<html>
@@ -16,5 +21,14 @@ post '/new' do
   6.times do 
     new_url << ('A'..'Z').to_a.sample
   end
-  "Your new URL is #{new_url.join} which redirects to #{params["url"]}"
+  new_url = new_url.join.to_s
+  #result = db.execute ( "select url_shortener.id, url_shortener.url from url_shortener where url_shortener.id = #{new_url}" )
+  #"#{result}"
+  "Your new URL is <a href=\"#{params["url"]}\">#{new_url}</a> which redirects to #{params["url"]}"
+  {
+    "id" => new_url,
+    "url" => params["url"],
+  }.each do |pair|
+    db.execute "insert into url_shortener values ( ?, ? )", pair
+  end
 end
