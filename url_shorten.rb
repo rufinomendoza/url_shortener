81254e989e949702ea8aa6cd71841adcd10710c3
rufinomendoza <ThinkPadT400@Mobile>
2012-07-30 09:15:16 -0400

    Got INSERT to work on DB

--- url_shorten.rb
+++ url_shorten.rb
@@ -2,7 +2,7 @@ require 'rubygems'
 require 'sinatra'
 require 'sqlite3'
 
-db = SQLite3::Database.new('url.sql')
+db = SQLite3::Database.new('url.db')
 db.execute( "create table if not exists url_shortener (id TEXT PRIMARY KEY, url TEXT);" )
 
 get '/new' do
@@ -22,13 +22,8 @@ post '/new' do
     new_url << ('A'..'Z').to_a.sample
   end
   new_url = new_url.join.to_s
-  #result = db.execute ( "select url_shortener.id, url_shortener.url from url_shortener where url_shortener.id = #{new_url}" )
-  #"#{result}"
+  db.execute "insert into url_shortener values ( ?, ? )", new_url, params["url"]
+  result = db.execute ( "select * from url_shortener" )
+  "#{result}"
   "Your new URL is <a href=\"#{params["url"]}\">#{new_url}</a> which redirects to #{params["url"]}"
-  {
-    "id" => new_url,
-    "url" => params["url"],
-  }.each do |pair|
-    db.execute "insert into url_shortener values ( ?, ? )", pair
-  end
 end
